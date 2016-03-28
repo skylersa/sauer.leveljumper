771ea9455654969b01c35e26e3d8f6962dfdad3d
Skyler Sauer <skyler@allen-sauer.com>
2016-03-27 17:47:38 -0700

    infinite level, spawned as you go

---
+++ Assets/LevelSpawnerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LevelSpawnerController : MonoBehaviour
+{
+	float LEVEL_SPACING = 3f;
+	float HORIZONTAL_FACTOR = 5f;
+	int PRESPAWN_COUNT = 3;
+
+	int count;
+	public GameObject levelprefab;
+	public Transform player;
+	float lastspawnheight;
+
+	void Start ()
+	{
+		lastspawnheight = player.position.y - LEVEL_SPACING;
+	}
+	
+
+	void Update ()
+	{
+		float playerheight = player.position.y;
+		if (playerheight + PRESPAWN_COUNT * LEVEL_SPACING > lastspawnheight) {
+			spawnnextlevel ();
+		}
+	}
+
+	void spawnnextlevel ()
+	{
+		lastspawnheight += LEVEL_SPACING;
+		GameObject level = Instantiate (levelprefab);
+		level.transform.position = new Vector3 (lastspawnheight * HORIZONTAL_FACTOR, lastspawnheight, 0);
+		level.GetComponentInChildren<TextMesh> ().text = "Level " + ++count;
+	}
+}
